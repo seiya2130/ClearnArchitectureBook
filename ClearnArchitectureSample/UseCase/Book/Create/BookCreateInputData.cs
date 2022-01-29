b92c67821f4a2b26e7ffe368d9701f2d5b20e678
Seiya_Satoh <satohseiya0312@gmail.com>
2022-01-29 17:30:19 +0900

    [add]UseCase,InputData追加

---
+++ ClearnArchitectureSample/UseCase/Book/Create/BookCreateInputData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCase.Book.Create
+{
+    public class BookCreateInputData
+    {
+        public string BookName { get; }
+
+        public BookCreateInputData(string bookName)
+        {
+            BookName = bookName;
+        }
+    }
+}
---
+++ ClearnArchitectureSample/UseCase/Book/Create/IBookCreateUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCase.Book.Create
+{
+    public interface IBookCreateUseCase
+    {
+        void Handle(BookCreateInputData inputData);
+    }
+}
