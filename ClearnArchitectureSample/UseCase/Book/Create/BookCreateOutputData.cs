f1cd44f3736cafd65d4229c27ba31b6252e8e095
Seiya_Satoh <satohseiya0312@gmail.com>
2022-02-01 08:44:12 +0900

    [add]Presenter,OutputData追加

---
+++ ClearnArchitectureSample/UseCase/Book/Create/BookCreateOutputData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCase.Book.Create
+{
+    public class BookCreateOutputData
+    {
+        public BookCreateOutputData(string bookId, DateTime created)
+        {
+            BookId = bookId;
+            Created = created;
+        }
+        public string BookId { get; }
+        public DateTime Created { get; }
+    }
+}
---
+++ ClearnArchitectureSample/UseCase/Book/Create/IBookCreatePresenter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCase.Book.Create
+{
+    interface IBookCreatePresenter
+    {
+        void Complete(BookCreateOutputData outputData);
+    }
+}
