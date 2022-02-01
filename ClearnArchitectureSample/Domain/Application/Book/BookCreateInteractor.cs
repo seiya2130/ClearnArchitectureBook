5fb5072c9b6d9d69dd66a7eb18ea22f913b479fd
Seiya_Satoh <satohseiya0312@gmail.com>
2022-02-01 09:02:00 +0900

    [add]Interactor追加

---
+++ ClearnArchitectureSample/Domain/Application/Book/BookCreateInteractor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UseCase.Book.Create;
+using Domain.Domain.Books;
+
+namespace Domain.Application.Book
+{
+    public class BookCreateInteractor : IBookCreateUseCase
+    {
+        private readonly IBookRepository bookRepository;
+        private readonly IBookCreatePresenter presenter;
+
+        public BookCreateInteractor(IBookRepository bookRepository, IBookCreatePresenter presenter)
+        {
+            this.bookRepository = bookRepository;
+            this.presenter = presenter;
+        }
+
+        public void Handle(BookCreateInputData inputData)
+        {
+            var bookName = inputData.BookName;
+            var duplicateBook = bookRepository.FindByBookName(bookName);
+
+            if(duplicateBook != null)
+            {
+                throw new Exception("duplicated");
+            }
+
+            var book = new Domain.Books.Book(bookName);
+            bookRepository.Save(book);
+
+            var outputData = new BookCreateOutputData(book.Id, DateTime.Now);
+            presenter.Complete(outputData);
+
+        }
+    }
+}
