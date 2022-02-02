768a2b4227d84f13bfd209756ab4af452b40cf43
Seiya_Satoh <satohseiya0312@gmail.com>
2022-02-02 09:19:18 +0900

    [add]BookRepository(MySQL)追加

---
+++ ClearnArchitectureSample/MySqlInfrastructure/Config.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MySqlInfrastructure
+{
+    public class Config
+    {
+        public const string ConnectionString = "Database=practice_clean_architecture;Data Source=localhost;User id=developer;Password=11111111;SslMode=none;";
+    }
+}
